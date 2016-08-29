6f146a2ac69a3fd19104d48206793928f63f757f
marshiyabensigar <marshiyabensigar@gmail.com>
2016-08-28 22:34:43 -0400

    Fixed IndexOutOfBoundsException

--- src/com/tek/interview/question/Foo.java
+++ src/com/tek/interview/question/Foo.java
@@ -146,7 +146,7 @@ class calculator {
 			double total = 0;
 
 			// Iterate through the items in the order
-			for (int i = 0; i <= r.size(); i++) {
+			for (int i = 0; i <= r.size()-1; i++) {
 
 				// Calculate the taxes
 				double tax = 0;
