c94d33f6e4b0c2b475a9a993fbd1f3260e7dc447
marshiyabensigar <marshiyabensigar@gmail.com>
2016-08-28 22:37:57 -0400

    To print the information in correct order

--- src/com/tek/interview/question/Foo.java
+++ src/com/tek/interview/question/Foo.java
@@ -1,9 +1,8 @@
 package com.tek.interview.question;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -187,7 +186,7 @@ public class Foo {
 
 	public static void main(String[] args) throws Exception {
 
-		Map<String, Order> o = new HashMap<String, Order>();
+		Map<String, Order> o = new LinkedHashMap<String, Order>();
 
 		Order c = new Order();
 
@@ -200,7 +199,7 @@ public class Foo {
 		o.put("Order 1", c);
 
 		// Reuse cart for an other order
-		c.clear();
+		c = new Order();
 
 		c.add(new OrderLine(new Item("imported box of chocolate", 10), 1));
 		c.add(new OrderLine(new Item("imported bottle of perfume", (float) 47.50), 1));
@@ -208,7 +207,7 @@ public class Foo {
 		o.put("Order 2", c);
 
 		// Reuse cart for an other order
-		c.clear();
+		c = new Order();
 
 		c.add(new OrderLine(new Item("Imported bottle of perfume", (float) 27.99), 1));
 		c.add(new OrderLine(new Item("bottle of perfume", (float) 18.99), 1));
